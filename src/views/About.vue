2a45ac6a4314d828e9a3a2b3ddd4adb6cce25d68
Martin Blascak <martin.blascak86@gmail.com>
2020-08-27 12:39:27 +0200

    fix form bug

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Meno</p>
+    <p>{{ name }}</p>
   </div>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Martin'
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="home">
     <p>Home</p>
-
-    <form class="card inputForm">
+    <p>{{ firstName }}</p>
+                              
+    <form class="card inputForm" @submit.prevent="addDataToFirestore">
       <div class="name">
         <label for="inputName">Meno:</label>
-        <input type="text" name="inputName">
+        <input type="text" name="inputName" v-model="firstName" required>
       </div>
       <div class="surname">
         <label for="inputSurname">Priezvisko:</label>
-        <input type="text" name="inputSurname">
+        <input type="text" name="inputSurname" v-model="lastName" required>
       </div>
       <div class="address">
         <label for="inputAddress">Adresa:</label>
-        <input type="text" name="inputAddress">
+        <input type="text" name="inputAddress" v-model="address" required>
       </div>
       <div class="comment">
         <label for="inputMessage">Popis podnetu:</label>
-        <textarea name="inputMessage" class="materialize-textarea"></textarea>
+        <textarea name="inputMessage" class="materialize-textarea" v-model="comment"></textarea>
       </div>
       <div class="picture">
         <label for="inputPicture">Obrázok:</label>
         <input type="file" name="inputPicture">
       </div>
-
 
       <button class="waves-effect waves-light btn">Odoslať</button>
     </form>
@@ -32,24 +32,45 @@
 </template>
 
 <script>
-
+import firestore from '@/firebase/init.js'
 
 export default {
   name: 'Home',
-}
-</script>
-
-
-
-
-
-
-
-<script>
-export default {
   data() {
     return {
-
+      firstName: '',
+      lastName: '',
+      address: '',
+      comment: '',
+      picture: null,
+    }
+  },
+  methods: {
+    mojalert() {
+      console.log(this.firstName)
+    },
+    addDataToFirestore() {
+      console.log(this.computedData)
+      firestore.collection('message')
+                .doc(Number(this.computedData.date).toString())
+                .set(this.computedData)
+                .then(() => {
+                  console.log('ok')
+                  console.log(Number(this.computedData.date).toString())
+                })
+                .catch((err) => console.log(err))
+      // firestore.collection('message')
+    },
+  },
+  computed: {
+    computedData() {
+      return {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        address: this.address,
+        comment: this.comment,
+        date: new Date()
+      }
     }
   }
 }
